4084202651f7347b4514c629bfd2a6ee753a0375
sali1502 <sali1502@sudent.miun.se>
2024-09-24 16:06:25 +0200

    Build.

--- src/js/StoreManager.ts
+++ src/js/StoreManager.ts
@@ -1,12 +1,14 @@
-// Funktionalitet för att spara och hämta kurser från localstorage
+/* Moment 1, DT208G TypeScript, Åsa Lindskog sali1502@student.miun.se */
+
+// Lagra och hämta kurser från localstorage
 import { Course } from "./course";
 
 export class storeManager {
-    // Sparar kurs i array i localstorage
+    // Spara kurs i array i localstorage
     static saveCourses(courses: Course[]) {
         localStorage.setItem("courses", JSON.stringify(courses))
     }
-    // Hämtar kurser i array från localstorage
+    // Hämta kurser i array från localstorage
     static loadCourses(): Course[] {
         const coursesStr = localStorage.getItem("courses");
         if (coursesStr) {
